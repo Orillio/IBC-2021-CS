d724455d2c544204c6fade5b5fb4de91b54c9eda
orillio <kopn48@mail.ru>
2021-08-03 18:31:23 +0300

    bubble sort

---
+++ Lab 2/bubble_sort_solution.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+
+void print_vector(const std::vector<int>& data){
+    for(int i : data){
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+}
+
+/* Sorting data in ascending order */
+void bubble_sort(std::vector<int>& data){
+    /* TODO: Implement bubble sort */
+    bool sorted;
+    do{
+        sorted = true;
+        for(int i = 0; i < data.size() - 1; i++){
+            if(data[i] > data[i+1]){
+                sorted = false;
+                int temp = data[i];
+                data[i] = data[i+1];
+                data[i+1] = temp;
+            }
+        }
+    }while(!sorted);
+
+}
+
+int main(void){
+    int n = 0;
+    do {
+        std::cout << "Input array size(positive number): ";
+        std::cin >> n;
+    } while(n < 1);
+
+    /* Reading array */
+    std::vector<int> data(n);
+    for(int i = 0; i < n; ++i){
+        std::cin >> data[i];
+    }
+
+    std::cout << "Original data: ";
+    print_vector(data);
+
+    auto start = std::chrono::high_resolution_clock::now();
+    bubble_sort(data);
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::nanoseconds duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+
+    std::cout << "Data after sorting: ";
+    print_vector(data);
+
+    std::cout << "Time taken: " << duration.count() << "ns" << std::endl;
+
+    return 0;
+}
